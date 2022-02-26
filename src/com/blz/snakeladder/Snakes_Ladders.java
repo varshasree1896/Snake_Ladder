abdb8d1c1ebd317d3427d1f684b8067956a1237f
Pooja Sree <you@example.com>
2022-02-27 00:54:55 +0530

    Added:UC1SinglePlayer

--- src/com/blz/snakeladder/Snakes_Ladders.java
+++ src/com/blz/snakeladder/Snakes_Ladders.java
@@ -1,7 +1,13 @@
 package com.blz.snakeladder;
 
+/**
+ * Snake And Ladder Game Project
+ */
+
 public class Snakes_Ladders {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake & Ladder Game");
+                int position = 0;
+                System.out.println("Position of player = " + position);
     }
 }
