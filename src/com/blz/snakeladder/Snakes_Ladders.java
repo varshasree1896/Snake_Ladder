62903d750d4048b8a5e7f4934b48dfaafdb34f38
Pooja Sree <you@example.com>
2022-02-28 00:11:43 +0530

    Added:UC4RepeatDiceTillEnd

--- src/com/blz/snakeladder/Snakes_Ladders.java
+++ src/com/blz/snakeladder/Snakes_Ladders.java
@@ -13,24 +13,35 @@ public class Snakes_Ladders {
 
     public static void main(String[] args) {
         System.out.println("Starting position of the player = " + position);
-        int dice = (int) ((Math.random()*10)%7);
-        int check = (int) ((Math.random()*10)%3);
-        switch (check) {
-            case SAME_POSITION:
-                System.out.println("Player on stay on same position");
-                break;
-            case LADDER:
-                position = position + dice;
-                System.out.println("Player got ladder and position of the player = " +position);
-            case SNAKE:
+        //Use while to iterate the values till end position reaches
+        while (position < 100) {
+            //Use random function to get dice value
+            int dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            //Use random function to check which case get to the player
+            int check = (int) ((Math.random() * 10) % 3);
+            switch (check) {
+                case SAME_POSITION:
+                    System.out.println("Player got same position");
+                    break;
+                case LADDER:
+                    position = position + dice;
+                    System.out.println("Player got ladder and position of the player = " + position);
+                    break;
+                case SNAKE:
+                    position = position - dice;
+                    //Use if function to avoid less than zero position of player
+                    if (position < 0) {
+                        position = 0;
+                        System.out.println("Player got snake and position of the player = " + position);
+                    }
+            }
+            // Use if function to restrict player to exceed 100
+            if (position > 100) {
                 position = position - dice;
-                if ( position < 0 ) {
-                    position = 0;
-                    System.out.println("Player got snake and position of the player = " +position);
-                }
+                System.out.println("Player on the same position = " + position);
+            }
         }
-
-
 
     }
 }
+
