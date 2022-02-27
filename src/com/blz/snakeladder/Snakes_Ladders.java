70e550002f9e471c87973f7818268fc0d3cfcdf8
Pooja Sree <you@example.com>
2022-02-28 00:02:33 +0530

    Added:UC3UsingSwitchCase

--- src/com/blz/snakeladder/Snakes_Ladders.java
+++ src/com/blz/snakeladder/Snakes_Ladders.java
@@ -5,12 +5,31 @@ package com.blz.snakeladder;
  */
 
 public class Snakes_Ladders {
+    static int position = 0;
+    public static final int SAME_POSITION = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake & Ladder Game");
-                int position = 0;
-                System.out.println("Position of player = " + position);
-                int dice =  (int) ((Math.random()*10)%7);
-        System.out.println("DIce number = " +dice);
+        System.out.println("Starting position of the player = " + position);
+        int dice = (int) ((Math.random()*10)%7);
+        int check = (int) ((Math.random()*10)%3);
+        switch (check) {
+            case SAME_POSITION:
+                System.out.println("Player on stay on same position");
+                break;
+            case LADDER:
+                position = position + dice;
+                System.out.println("Player got ladder and position of the player = " +position);
+            case SNAKE:
+                position = position - dice;
+                if ( position < 0 ) {
+                    position = 0;
+                    System.out.println("Player got snake and position of the player = " +position);
+                }
+        }
+
 
 
     }
