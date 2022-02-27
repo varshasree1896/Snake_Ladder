a5a0148baa9f4c9c96b5967a8f728b64d06f2b0d
Pooja Sree <you@example.com>
2022-02-28 00:21:02 +0530

    Added:UC5EnsureExactEndPosotion

--- src/com/blz/snakeladder/Snakes_Ladders.java
+++ src/com/blz/snakeladder/Snakes_Ladders.java
@@ -40,6 +40,9 @@ public class Snakes_Ladders {
                 position = position - dice;
                 System.out.println("Player on the same position = " + position);
             }
+            if ( position == 100 ) {
+                System.out.println("Player won the game");
+            }
         }
 
     }
